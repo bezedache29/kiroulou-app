2d91a6fdf9d2d2fa881a483f8d7bed7da6a2b25d
Tophe <bezedache29@gmail.com>
2022-08-10 10:08:07 +0200

    Intégration EditCommentScreen

--- src/screens/comments/CommentsScreen.jsx
+++ src/screens/comments/CommentsScreen.jsx
@@ -7,9 +7,12 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import { useTheme } from 'react-native-paper'
+
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
@@ -26,6 +29,10 @@ import {
 import CustomContainer from '../../components/CustomContainer'
 import useFaker from '../../hooks/useFaker'
 import useUtils from '../../hooks/useUtils'
+import CustomBSModal from '../../components/CustomBSModal'
+import ButtonBS from '../../components/ButtonBS'
+import CustomAlert from '../../components/CustomAlert'
+import CustomOverlay from '../../components/CustomOverlay'
 
 const CommentsScreen = ({ navigation, route }) => {
   const { colors } = useTheme()
@@ -36,6 +43,10 @@ const CommentsScreen = ({ navigation, route }) => {
 
   const [comments, setComments] = useState([])
   const [comment, setComment] = useState('')
+  const [myPost, setMyPost] = useState(true) // Pour intégration / tests
+  const [overlay, setOverlay] = useState(false)
+  const [showDeleteComment, setShowDeleteComment] = useState(false)
+  const [commentBS, setCommentBS] = useState(false)
 
   useEffect(() => {
     for (let i = 0; i < 3; i++) {
@@ -43,109 +54,191 @@ const CommentsScreen = ({ navigation, route }) => {
     }
   }, [])
 
-  return (
-    <CustomContainer label="Commentaires" pressBack={() => navigation.goBack()}>
-      <View style={{ flex: 1, backgroundColor: colors.backgroundNav }}>
-        <FlatList
-          style={styles.flatlist}
-          data={comments}
-          showsVerticalScrollIndicator={false}
-          ListEmptyComponent={() => (
-            <View style={{ flex: 1 }}>
-              <Text
-                style={[littleTitle, textAlignCenter, { color: colors.text }]}
-              >
-                Pas de commentaires
-              </Text>
-            </View>
-          )}
-          renderItem={({ item }) => (
-            <View
-              style={[
-                styles.renderItem,
-                {
-                  backgroundColor: colors.backgroundBox,
-                },
-              ]}
-            >
-              <View style={[rowCenter]}>
-                <TouchableOpacity onPress={() => {}} style={styles.header}>
-                  <ImageBackground
-                    source={{
-                      uri: item.avatar,
-                    }}
-                    style={styles.avatar}
-                    imageStyle={styles.avatarStyle}
-                  />
-                  <View style={ml20}>
-                    <Text style={[littleTitle, { color: colors.text }]}>
-                      {item.firstname} {item.lastname}
-                    </Text>
-                    <Text style={[defaultText, { color: colors.text }]}>
-                      {item.club ? item.club : item.hike}
-                    </Text>
-                  </View>
-                </TouchableOpacity>
+  // Ref pour la bottomSheet
+  const bottomSheetRef = useRef(null)
 
-                <TouchableOpacity
-                  onPress={() => {}}
-                  style={{ marginLeft: 'auto' }}
+  // Permet d'ouvrir et fermer la bottomSheet pour afficher les options de l'article
+  const toggleBottomSheet = (item) => {
+    if (overlay) {
+      setOverlay(false)
+      setCommentBS(false)
+      bottomSheetRef?.current?.closeBottomSheet()
+    } else {
+      setOverlay(true)
+      setCommentBS(item)
+      bottomSheetRef?.current?.openBottomSheet()
+    }
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheetModalProvider>
+        <CustomContainer
+          label="Commentaires"
+          pressBack={() => navigation.goBack()}
+        >
+          {overlay && <CustomOverlay />}
+
+          <View style={{ flex: 1, backgroundColor: colors.backgroundNav }}>
+            <FlatList
+              style={styles.flatlist}
+              data={comments}
+              showsVerticalScrollIndicator={false}
+              ListEmptyComponent={() => (
+                <View style={{ flex: 1 }}>
+                  <Text
+                    style={[
+                      littleTitle,
+                      textAlignCenter,
+                      { color: colors.text },
+                    ]}
+                  >
+                    Pas de commentaires
+                  </Text>
+                </View>
+              )}
+              renderItem={({ item }) => (
+                <View
+                  style={[
+                    styles.renderItem,
+                    {
+                      backgroundColor: colors.backgroundBox,
+                    },
+                  ]}
                 >
+                  <View style={[rowCenter]}>
+                    <TouchableOpacity onPress={() => {}} style={styles.header}>
+                      <ImageBackground
+                        source={{
+                          uri: item.avatar,
+                        }}
+                        style={styles.avatar}
+                        imageStyle={styles.avatarStyle}
+                      />
+                      <View style={ml20}>
+                        <Text style={[littleTitle, { color: colors.text }]}>
+                          {item.firstname} {item.lastname}
+                        </Text>
+                        <Text style={[defaultText, { color: colors.text }]}>
+                          {item.club ? item.club : item.hike}
+                        </Text>
+                      </View>
+                    </TouchableOpacity>
+
+                    {/* Icon et fonction différente si c'est mon commentaire ou non */}
+                    {myPost ? (
+                      <TouchableOpacity
+                        onPress={() => toggleBottomSheet(item)}
+                        style={{ marginLeft: 'auto' }}
+                      >
+                        <MaterialCommunityIcons
+                          name="cog-outline"
+                          size={30}
+                          color={colors.text}
+                        />
+                      </TouchableOpacity>
+                    ) : (
+                      <TouchableOpacity
+                        onPress={() => {}}
+                        style={{ marginLeft: 'auto' }}
+                      >
+                        <MaterialCommunityIcons
+                          name="star-outline"
+                          size={30}
+                          color={colors.text}
+                        />
+                      </TouchableOpacity>
+                    )}
+                  </View>
+
+                  <Text
+                    style={[
+                      defaultText,
+                      my10,
+                      { color: colors.text, fontSize: 16 },
+                    ]}
+                  >
+                    {formatDate(item.date)}
+                  </Text>
+
+                  <Text style={[defaultText, { color: colors.text }]}>
+                    {item.message}
+                  </Text>
+                </View>
+              )}
+              keyExtractor={(item) => item.id}
+            />
+
+            {/* Input pour ajouter un commentaire */}
+            <View style={styles.inputContainer}>
+              <View style={[rowCenter]}>
+                <TextInput
+                  onChangeText={setComment}
+                  value={comment}
+                  placeholder="Votre commentaire ..."
+                  placeholderTextColor={colors.text}
+                  style={[
+                    defaultText,
+                    {
+                      backgroundColor: colors.background,
+                      borderColor: colors.border,
+                      color: colors.text,
+                    },
+                    styles.inputComment,
+                  ]}
+                />
+
+                {/* IconSend Commentaire */}
+                <TouchableOpacity onPress={() => {}} style={styles.iconSend}>
                   <MaterialCommunityIcons
-                    name="star-outline"
-                    size={30}
-                    color={colors.text}
+                    name="send"
+                    size={33}
+                    color={comment !== '' ? darkPrimaryColor : colors.text}
                   />
                 </TouchableOpacity>
               </View>
-
-              <Text
-                style={[
-                  defaultText,
-                  my10,
-                  { color: colors.text, fontSize: 16 },
-                ]}
-              >
-                {formatDate(item.date)}
-              </Text>
-
-              <Text style={[defaultText, { color: colors.text }]}>
-                {item.message}
-              </Text>
             </View>
-          )}
-          keyExtractor={(item) => item.id}
-        />
-
-        <View style={styles.inputContainer}>
-          <View style={[rowCenter]}>
-            <TextInput
-              onChangeText={setComment}
-              value={comment}
-              placeholder="Votre commentaire ..."
-              placeholderTextColor={colors.text}
-              style={[
-                defaultText,
-                {
-                  backgroundColor: colors.background,
-                  borderColor: colors.border,
-                  color: colors.text,
-                },
-                styles.inputComment,
-              ]}
-            />
-
-            <TouchableOpacity onPress={() => {}} style={styles.iconSend}>
-              <MaterialCommunityIcons
-                name="send"
-                size={33}
-                color={comment !== '' ? darkPrimaryColor : colors.text}
-              />
-            </TouchableOpacity>
           </View>
-        </View>
-      </View>
-    </CustomContainer>
+
+          {/* BottomSheet pour options du commentaire */}
+          <CustomBSModal
+            title="Que souhaitez vous faire ?"
+            SP={['25%', '30%']}
+            ref={bottomSheetRef}
+            onDismiss={() => {
+              bottomSheetRef?.current?.closeBottomSheet()
+              setOverlay(false)
+              setCommentBS(false)
+            }}
+          >
+            <ButtonBS onPress={() => setShowDeleteComment(true)} cancel>
+              Supprimer le commentaire
+            </ButtonBS>
+            <ButtonBS
+              onPress={() => {
+                bottomSheetRef?.current?.closeBottomSheet()
+                setOverlay(false)
+                navigation.navigate('EditComment', {
+                  editComment: commentBS,
+                })
+              }}
+            >
+              Modifier le commentaire
+            </ButtonBS>
+          </CustomBSModal>
+
+          {/* Alert du suppression de commentaire */}
+          <CustomAlert
+            showAlert={showDeleteComment}
+            title="Attention !"
+            message="Etes vous sur de vouloir supprimer le commentaire ?"
+            onDismiss={() => setShowDeleteComment(false)}
+            onCancelPressed={() => setShowDeleteComment(false)}
+            onConfirmPressed={() => setShowDeleteComment(false)}
+          />
+        </CustomContainer>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   )
 }
 
